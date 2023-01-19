70d85d1a65e02c520616e5b8e66e95ce3d92297d
momodaGit <60724039+momodaGit@users.noreply.github.com>
2023-01-19 17:45:41 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||星|辰|新|年|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||龟|友|们|新|年|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
